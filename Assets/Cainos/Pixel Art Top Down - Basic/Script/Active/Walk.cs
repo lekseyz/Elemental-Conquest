c54bdab3a97474eb0805af5855fddd7c69c0b20e
Chertila112 <sukhin636@gmail.com>
2023-10-30 15:03:08 +0300

    Movement changes + PunchingDummy sprite added
    Added dash, fixed diagonal movement speed

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/Active/Walk.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/Active/Walk.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 public class Walk : MonoBehaviour
 {
@@ -11,8 +12,20 @@ public class Walk : MonoBehaviour
     private bool StayRight;
     private bool StayLeft;
     private bool StayDown;
+
+    private float activeSpeed;
+    public float dashSpeed;
+
+    public float dashDuration = 0.5f;
+    public float dashCooldown = 2f;
+
+    private float dashCounter=0;
+    private float dashCoolCounter=0;
+
+    
     private void Start()
     {
+        activeSpeed = speed;
         StayLeft = false;
         StayRight = false;
         StayUp = false;
@@ -33,7 +46,7 @@ public class Walk : MonoBehaviour
             StayUp = false;
             StayDown = false;
         }
-
+        movement.Normalize();
         if ((movement.y == 1) && (StayLeft == false) && (StayRight == false) && (StayDown == false))
             StayUp = true;
         if ((movement.x == -1) && (StayUp == false) && (StayRight == false) && (StayDown == false))
@@ -50,9 +63,30 @@ public class Walk : MonoBehaviour
         animator.SetBool("StayRight", StayRight);
 
         animator.SetBool("StayDown", StayDown);
-    }
-    private void FixedUpdate()
+        //Dash implementation
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if ((dashCoolCounter<=0)&&(dashCounter<=0))
+            {
+                activeSpeed=dashSpeed;
+                dashCounter=dashDuration;
+            }
+        }
+        if (dashCounter>0)
+        {
+            dashCounter-=Time.deltaTime;
+            dashCoolCounter = dashCooldown;
+        }
+        else
+            activeSpeed = speed;
+        if (dashCoolCounter>0)
+        {
+            dashCoolCounter-=Time.deltaTime;
+        }
+
+}
+private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * activeSpeed * Time.fixedDeltaTime);
     }
 }
