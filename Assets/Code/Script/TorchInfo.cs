01c35624ac392ccdce776ba826b33e5b0c2d06d2
egorsheremet <81770845+egorsheremet@users.noreply.github.com>
2024-04-03 00:07:20 +0300

    UI

---
+++ Assets/Code/Script/TorchInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchInfo : MonoBehaviour
+{
+    [SerializeField] private GameObject image;
+    [SerializeField] private Transform hero;
+    [SerializeField] private float dist;
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, hero.position) < dist)
+            image.SetActive(true);
+        else
+            image.SetActive(false);
+    }
+
+}
