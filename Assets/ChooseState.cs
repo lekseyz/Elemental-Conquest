2dd18599e7173f6987bc38f67fc664084aee0b10
EvgS <sukhin636@gmail.com>
2024-05-18 12:07:10 +0300

    boss attack hitbox fixed

--- Assets/ChooseState.cs
+++ Assets/ChooseState.cs
@@ -6,7 +6,7 @@ public class ChooseState : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        int rand = 1;//Random.Range(0, 2);
+        int rand = Random.Range(0, 2);
 
         if (rand == 0)
         {
