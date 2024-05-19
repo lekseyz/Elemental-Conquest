465a1ab99607a483533d9dd5a3454414c1966397
EvgS <sukhin636@gmail.com>
2024-05-19 22:43:45 +0300

    Update Knight.cs

--- Assets/Knight.cs
+++ Assets/Knight.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Knight : Interactable
 {
     private PatrolerKnight patroler;
+    private SpriteRenderer spriteRenderer;
     public override void applyFireBall()
     {
         patroler.takeDamage(25);
+        spriteRenderer.color = Color.red;
+        StartCoroutine(redTime(spriteRenderer));
     }
-
+    IEnumerator redTime(SpriteRenderer sp)
+    {
+        yield return new WaitForSeconds(0.1f);
+        sp.color = Color.white;
+    }
     public override void applyStone()
     {
         patroler.takeDamage(0);
@@ -25,6 +32,7 @@ public class Knight : Interactable
     void Start()
     {
         patroler = gameObject.GetComponent<PatrolerKnight>();
+        spriteRenderer = patroler.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
